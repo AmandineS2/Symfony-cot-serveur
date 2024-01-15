092629513b45230784f31f21af7e8357fdb09fcb
AmandineS2 <amandine.glaudel@etudiant.univ-reims.fr>
2024-01-15 16:58:26 +0100

    ça marche ! Et sans pieds en plus !

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,64 +5,60 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker;
+use DateTimeImmutable;
 use App\Entity\Actor;
 use App\Entity\Movie;
-use DateTimeImmutable;
-use App\Entity\User;
-
-use phpDocumentor\Reflection\Types\Null_;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-    $faker =\Faker\Factory::create();
-    $faker->addProvider(new \Xylis\FakerCinema\Provider\Person($faker));
 
-    $actor = $faker->actors($gender = null, $count = 100, $duplicates = false);
-    foreach ($actor as $item) {
+        $faker = \Faker\Factory::create();
+        $faker->addProvider(new \Xylis\FakerCinema\Provider\Person($faker));
 
-        $fullname = $item;
-        $fullnameExploded = explode(' ', $fullname);
-        $firstname = $fullnameExploded[0];
-        $lastname = $fullnameExploded[1];
+        $actors = $faker->actors($gender = null, $count = 190, $duplicates = false);
+        $createdActors = [];
+        foreach ($actors as $item) {
+            $fullname = $item; //ex : Christian Bale
+            $fullnameExploded = explode(' ', $fullname);
 
-        for($i =0; $i<5; $i++) {
-            $movie = new Movie();
-            $movie
-                ->setTitle("Mon film ($i)")
-                ->setDescription("ma description")
-                ->setDirector("toto")
-                ->setReleaseDate(new \DateTime())
-            ;
-            $manager->persist($movie);
-        
+            $firstname = $fullnameExploded[0]; //ex : Christian
+            $lastname = $fullnameExploded[1]; //ex : Bale
+
+            $actor = new Actor();
+            $actor->setLastname($lastname);
+            $actor->setFirstname($firstname);
+            $actor->setDate($faker->dateTimeThisCentury());
+            $actor->setCreatedAt(new DateTimeImmutable());
+            $actor->setNationality("fr");
+//            $actor->setReleaseDate(new DateTimeImmutable());
+
+            $createdActors[] = $actor;
+
+            $manager->persist($actor);
         }
 
-        $actor = new Actor();
-        $actor->setLastname($lastname);
-        $actor->setFirstname($firstname);
-		$actor->setDate(new \DateTime());
-        $actor->setCreatedAt(new \DateTimeImmutable());
-        $actor->setNationality('Germany');
-        $manager->persist($actor);
+        $faker->addProvider(new \Xylis\FakerCinema\Provider\Movie($faker));
+        $movies = $faker->movies(100);
+        foreach ($movies as $item) {
+            $movie = new Movie();
+            $movie->setTitle($item);
+            $movie->setReleaseDate(new DateTimeImmutable());
+            $movie->setDescription("la description");
+            $movie->setDirector("nul");
+           
+
+            shuffle($createdActors);
+            $createdActorsSliced = array_slice($createdActors, 0, 4);
+            foreach ($createdActorsSliced as $actor) {
+                $movie->addActor($actor);
+            }
+            $manager->persist($movie);
+        }
+
+
         $manager->flush();
+        return true;
     }
-
-    
-        
-    }
-
-   
-
-  //  $faker = \Faker\Factory::create();
-  //  $faker->addProvider(new \Xylis\FakerCinema\Provider\Movie($faker));
-
-  
-
-
-
-
-  
-
 }
